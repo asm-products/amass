e0c9368e39fec13e863b83a386887e892f2b7c5a
Dominic Goulet <dominic.goulet@gmail.com>
2014-04-09 08:58:21 -0400

    Minor improvements

--- Gemfile
+++ Gemfile
@@ -1,15 +1,16 @@
 source 'https://rubygems.org'
 
-ruby '2.1.1'
+ruby '2.0.0'
 
 gem 'rails', '4.1.0'
 
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'pg'
-gem 'puma'
+gem 'unicorn'
 gem 'sass-rails', '~> 4.0.1'
 gem 'uglifier', '>= 1.3.0'
+gem 'figaro'
 
 group :development, :test do
   gem 'foreman'
@@ -18,4 +19,12 @@ end
 
 group :development do
   gem 'spring'
+  gem 'better_errors'
+  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
+  gem 'quiet_assets'
+  gem 'rails_layout'
+end
+
+group :production do
+  gem 'rails_12factor'
 end
---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+# config/unicorn.rb
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    logger.debug 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    logger.debug 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
